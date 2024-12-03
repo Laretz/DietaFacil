984acd826363a2db54d530c5dc05bd99d0fd127a
Renato Ramon <renatophoenix123@gmail.com>
2024-12-03 09:55:38 -0300

    feat(mobile): initialize mobile project with React Native
    - Set up the initial structure for the mobile application
    - Added basic dependencies
    - Created initial screen

---
+++ mobile/app/step/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native'
+import { colors } from '../../constants/colors'
+import { Header } from '../../components/header'
+
+export default function Step(){
+    return (
+        <View>
+         
+          <Header
+        step='Passo 1'
+        title='Vamos começar'
+      />
+        </View>
+    )
+}
